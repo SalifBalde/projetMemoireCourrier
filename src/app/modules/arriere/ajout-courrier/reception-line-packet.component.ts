59e91b0b54f247c49eb86b34914ad2d894a3790b
mdiokou <mdiokou@gmail.com>
2025-05-06 12:51:17 +0000

    from balde

--- src/app/modules/arriere/ajout-courrier/reception-line-packet.component.ts
+++ src/app/modules/arriere/ajout-courrier/reception-line-packet.component.ts
@@ -358,7 +358,6 @@ export class ReceptionLinePacketComponent implements  OnInit {
                     courrier.userId= this.iduser
                 }
 
-                console.log(courrier)
             })
             this.courrierService
                 .updateCourriers(this.courrierss)
